8b0f2625e3925bd2b3b0ebbd3782ec4405cb1415
debraj chakraborty <DebrajCh@gmail.com>
2021-05-29 11:02:47 -0400

    Test - No build

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -41,43 +41,48 @@ namespace MyZipWeather.Controllers
         public IActionResult GetWeather(City inputCity)
         {
             //Assign API KEY which received from OPENWEATHERMAP.ORG  
-            string appId = "";
+            //string appId = "";
 
-            //API path with CITY parameter and other parameters.  
-            string url = string.Format
-                ("http://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&cnt=1&APPID={1}", inputCity.CityName, appId);
+            ////API path with CITY parameter and other parameters.  
+            //string url = string.Format
+            //    ("http://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&cnt=1&APPID={1}", inputCity.CityName, appId);
 
-            using (WebClient client = new WebClient())
-            {
-                string json = client.DownloadString(url);
+            //using (WebClient client = new WebClient())
+            //{
+            //    string json = client.DownloadString(url);
 
-                //Converting to OBJECT from JSON string.  
-                RootObject weatherInfo = JsonSerializer.Deserialize<RootObject>(json);
+            //    //Converting to OBJECT from JSON string.  
+            //    RootObject weatherInfo = JsonSerializer.Deserialize<RootObject>(json);
 
-                //Special VIEWMODEL design to send only required fields not all fields which received from   
-                //www.openweathermap.org api  
-                ResultViewModel rslt = new ResultViewModel();
+            //    //Special VIEWMODEL design to send only required fields not all fields which received from   
+            //    //www.openweathermap.org api  
+            //    ResultViewModel rslt = new ResultViewModel();
 
-                rslt.Country = weatherInfo.sys.country;
-                rslt.City = weatherInfo.name;
-                rslt.Lat = Convert.ToString(weatherInfo.coord.lat);
-                rslt.Lon = Convert.ToString(weatherInfo.coord.lon);
-                rslt.Description = weatherInfo.weather[0].description;
-                rslt.Humidity = Convert.ToString(weatherInfo.main.humidity);
-                rslt.Temp = Convert.ToString(weatherInfo.main.temp);
-                rslt.TempFeelsLike = Convert.ToString(weatherInfo.main.feels_like);
-                rslt.TempMax = Convert.ToString(weatherInfo.main.temp_max);
-                rslt.TempMin = Convert.ToString(weatherInfo.main.temp_min);
-                rslt.WeatherIcon = weatherInfo.weather[0].icon;
+            //    rslt.Country = weatherInfo.sys.country;
+            //    rslt.City = weatherInfo.name;
+            //    rslt.Lat = Convert.ToString(weatherInfo.coord.lat);
+            //    rslt.Lon = Convert.ToString(weatherInfo.coord.lon);
+            //    rslt.Description = weatherInfo.weather[0].description;
+            //    rslt.Humidity = Convert.ToString(weatherInfo.main.humidity);
+            //    rslt.Temp = Convert.ToString(weatherInfo.main.temp);
+            //    rslt.TempFeelsLike = Convert.ToString(weatherInfo.main.feels_like);
+            //    rslt.TempMax = Convert.ToString(weatherInfo.main.temp_max);
+            //    rslt.TempMin = Convert.ToString(weatherInfo.main.temp_min);
+            //    rslt.WeatherIcon = weatherInfo.weather[0].icon;
 
-                City c = new City();
-                c.CityName = rslt.City;
-                c.HighestTemp = rslt.TempMax;
-                return View("Index",c);
+            //    City c = new City();
+            //    c.CityName = "India";
+            //    c.HighestTemp = rslt.TempMax;
+            //    return View("Index",c);
 
 
-            }
+            //}
+
+            City c = new City();
+            c.CityName = "India";
+            c.HighestTemp = "100C";
+            return View("Index", c);
         }
-        
+
     }
 }
