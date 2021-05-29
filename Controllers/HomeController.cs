0cddc3233248b5cf2be7bcb9a377e72ab3461fac
debraj chakraborty <DebrajCh@gmail.com>
2021-05-29 04:33:21 -0400

    API Integration

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 using MyZipWeather.Models;
+using System.Net;
 
 namespace MyZipWeather.Controllers
 {
@@ -20,7 +22,8 @@ namespace MyZipWeather.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            City city = new City();
+            return View(city);
         }
 
         public IActionResult Privacy()
@@ -33,5 +36,48 @@ namespace MyZipWeather.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpPost]
+        public IActionResult GetWeather(City inputCity)
+        {
+            //Assign API KEY which received from OPENWEATHERMAP.ORG  
+            string appId = "23ab8919299bbf22083b63e6eb8c8859";
+
+            //API path with CITY parameter and other parameters.  
+            string url = string.Format
+                ("http://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&cnt=1&APPID={1}", inputCity.CityName, appId);
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+
+                //Converting to OBJECT from JSON string.  
+                RootObject weatherInfo = JsonSerializer.Deserialize<RootObject>(json);
+
+                //Special VIEWMODEL design to send only required fields not all fields which received from   
+                //www.openweathermap.org api  
+                ResultViewModel rslt = new ResultViewModel();
+
+                rslt.Country = weatherInfo.sys.country;
+                rslt.City = weatherInfo.name;
+                rslt.Lat = Convert.ToString(weatherInfo.coord.lat);
+                rslt.Lon = Convert.ToString(weatherInfo.coord.lon);
+                rslt.Description = weatherInfo.weather[0].description;
+                rslt.Humidity = Convert.ToString(weatherInfo.main.humidity);
+                rslt.Temp = Convert.ToString(weatherInfo.main.temp);
+                rslt.TempFeelsLike = Convert.ToString(weatherInfo.main.feels_like);
+                rslt.TempMax = Convert.ToString(weatherInfo.main.temp_max);
+                rslt.TempMin = Convert.ToString(weatherInfo.main.temp_min);
+                rslt.WeatherIcon = weatherInfo.weather[0].icon;
+
+                City c = new City();
+                c.CityName = rslt.City;
+                c.HighestTemp = rslt.TempMax;
+                return View("Index",c);
+
+
+            }
+        }
+        
     }
 }
---
+++ Models/City.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyZipWeather.Models
+{
+    public class City
+    {
+        public string PinCode { get; set; }
+
+        [DisplayName("City")]
+        public string CityName { get; set; }
+        [DisplayName("Max Temperature")]
+        public string HighestTemp { get; set; }
+    }
+}
